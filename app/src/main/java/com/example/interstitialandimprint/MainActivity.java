db404e534f1096da10b223e351d991ce9aa2677b
Glebannister <igi2000@inbox.ru>
2020-06-22 18:35:44 +0300

    feat: device id

--- app/src/main/java/com/example/interstitialandimprint/MainActivity.java
+++ app/src/main/java/com/example/interstitialandimprint/MainActivity.java
@@ -4,8 +4,13 @@ import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.InterstitialAd;
 
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
@@ -13,18 +18,31 @@ import android.widget.Button;
 
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
 
 
+    private static final String KEY_STORE = "1";
     private Button button1;
+    private Button button2;
+    private FingerprintManager fingerprintManager;
     private InterstitialAd mInterstitialAd;
+    private String id;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
         button1.setEnabled(false);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +54,13 @@ public class MainActivity extends AppCompatActivity {
         mInterstitialAd = newInterstitialAd();
         loadInterstitial();
 
+        id = UUID.randomUUID().toString();
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showId();
+            }
+        });
     }
 
 
@@ -80,5 +105,19 @@ public class MainActivity extends AppCompatActivity {
     private void goToNextLevel() {
         mInterstitialAd = newInterstitialAd();
         loadInterstitial();
+    }
+
+    private static boolean initKeyStore() {
+        try {
+            KeyStore keyStore = KeyStore.getInstance(KEY_STORE);
+            keyStore.load(null);
+            return true;
+        } catch (KeyStoreException | IOException | NoSuchAlgorithmException | CertificateException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    private void showId(){
+        Toast.makeText(this, id, Toast.LENGTH_LONG).show();
     }
 }
