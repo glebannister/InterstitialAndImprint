540a337279d7b64f1fad111abfaf70333cf00ea2
Glebannister <igi2000@inbox.ru>
2020-06-22 11:20:40 +0300

    feat: interstitial implementation

---
+++ app/src/main/java/com/example/interstitialandimprint/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.interstitialandimprint;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button button1;
+    private InterstitialAd mInterstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.button1);
+        button1.setEnabled(false);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showInterstitial();
+            }
+        });
+
+        mInterstitialAd = newInterstitialAd();
+        loadInterstitial();
+
+    }
+
+
+    private InterstitialAd newInterstitialAd() {
+        InterstitialAd interstitialAd = new InterstitialAd(this);
+        interstitialAd.setAdUnitId(getString(R.string.interstitial_id));
+        interstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                button1.setEnabled(true);
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                button1.setEnabled(true);
+            }
+
+            @Override
+            public void onAdClosed() {
+                goToNextLevel();
+            }
+        });
+        return interstitialAd;
+    }
+
+    private void showInterstitial() {
+        if (mInterstitialAd != null && mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            Toast.makeText(this, "Ad did not load", Toast.LENGTH_SHORT).show();
+            goToNextLevel();
+        }
+    }
+
+    private void loadInterstitial() {
+        button1.setEnabled(false);
+        AdRequest adRequest = new AdRequest.Builder()
+                .setRequestAgent("android_studio:ad_template").build();
+        mInterstitialAd.loadAd(adRequest);
+    }
+
+    private void goToNextLevel() {
+        mInterstitialAd = newInterstitialAd();
+        loadInterstitial();
+    }
+}
